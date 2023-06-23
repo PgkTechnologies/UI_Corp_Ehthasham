e832551f85413e1a8e45b2cec6e3b749e92505f7
Mohammed Shoeb Uddin <117268285+Shoeb07@users.noreply.github.com>
2023-06-23 17:03:04 +0530

    Omcampus

--- corp-v2/src/Pages/Jobs/Jobs.js
+++ corp-v2/src/Pages/Jobs/Jobs.js
@@ -1,23 +1,56 @@
+import { useState } from "react";
+import OnCampusJobs from "./onCampusJobs";
+import OffCampusJobs from "./offCampusJobs";
 
 
 const Jobs = () => {
+
+    const [goOnCampus, setGoOnCampus] = useState(false);
+    const [goOffCampus, setGoOffCampus] = useState(false);
+
+    const handleCampusDrive = (mode) => {
+        console.log(mode, 'driveMode')
+        if (mode === 'ONCAM') {
+            setGoOnCampus(true);
+            setGoOffCampus(false);
+            
+        } else if (mode === 'OFFCAM') {
+            setGoOffCampus(true);
+            setGoOnCampus(false);
+        }
+
+    }
+
     return (
-        // <div style={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center', background: 'black', color: 'whitesmoke' }}>
-        //     JObs Page
-        // </div>
-        <div style={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center', marginTop: '55px', position: 'relative' }}>
-            <img src="/images/corporateIcons/JobsPage.svg" alt="Example" style={{ maxWidth: '100%', maxHeight: '100%', zIndex: '-1' }} />
+        <>
+            <div style={{ display: 'flex', width: '35%', alignItems: 'center', justifyContent: 'center', marginTop: '55px', position: 'relative' }}>
+                <img src="/images/corporateIcons/JobsPage.svg" alt="Example" style={{ maxWidth: '100%', maxHeight: '100%', zIndex: '-1' }} />
 
-            <div style={{ position: 'absolute', bottom: '110px', left: '230px' }}>
-                <button className="step-card tab-select" style={{ display: 'flex', marginLeft: '15px' }}>ON Campus Drive</button>
+                <div style={{ position: 'absolute', bottom: '110px', left: '30px' }}>
+                    <button className="step-card tab-select"
+                        style={{ display: 'flex', marginLeft: '15px' }}
+                        onClick={() => { handleCampusDrive('ONCAM') }}
+                    >ON Campus Drive</button>
 
+                </div>
+
+                <div style={{ position: 'absolute', bottom: '110px', right: '30px' }}>
+                    <button className="step-card tab-select"
+                        style={{ display: 'flex' }}
+                        onClick={() => { handleCampusDrive('OFFCAM') }}
+                    >
+                        OFF Campus Drive</button>
+
+                </div>
             </div>
 
-            <div   style={{ position: 'absolute', bottom: '110px', right: '230px' }}>
-                <button className="step-card tab-select"  style={{ display: 'flex' }}>OFF Campus Drive</button>
-
-            </div>
-        </div>
+            {goOnCampus && (
+                <OnCampusJobs/>
+            )}
+            {goOffCampus && (
+                <OffCampusJobs/>
+            )}
+        </>
     )
 }
 
